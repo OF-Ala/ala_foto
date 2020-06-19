c04d28250baf0c53f2aa1c595686d7e24b306691
OF-Ala <66063022+OF-Ala@users.noreply.github.com>
2020-06-20 00:34:00 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import logging
+import choko_model
+import time
 
 from aiogram import Bot, Dispatcher, executor, types
 
@@ -10,20 +12,28 @@ logging.basicConfig(level=logging.INFO)
 # Initialize bot and dispatcher
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
+save_path = 'data/'
+model = choko_model.Choko_transform_model(save_path)
 
 @dp.message_handler(commands=['start', 'help'])
 async def send_welcome(message: types.Message):
     """
     This handler will be called when user sends `/start` or `/help` command
     """
-    await message.reply("Hi!\nI'm EchoBot!\nPowered by aiogram.")
+    await message.reply("Hi!\nI'm AlaFotoBot! I can make your foto look more choko!\nSend me a foto and see yourself!")
 
 @dp.message_handler(content_types=['photo'])
 async def foto_input(message: types.Message):
     # old style:
     # await bot.send_message(message.chat.id, message.text)
-    message.photo[-1].download('test.jpg')
-    await message.answer('Nice foto!')
+    await message.answer('Nice foto! Wait a sec, we are transforming it.')
+    ms = int(time.time())
+
+    file_name = 'foto' + ms + '.jpg'
+    message.photo[-1].download(save_path + file_name)
+    model.Transform_to_choko(file_name)
+    await message.answer_photo(save_path + 'conv_'+ file_name)
+
 
 @dp.message_handler()
 async def echo(message: types.Message):
