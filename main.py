de7aa264a7c49ba9834d65a8f91b7fb4c0096adb
OF-Ala <66063022+OF-Ala@users.noreply.github.com>
2020-06-21 02:36:44 +0300

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ USE_WEBHOOK = os.environ['USE_WEBHOOK']
 
 # webhook settings
 WEBHOOK_HOST = 'https://ala-foto-bot.herokuapp.com/'
-WEBHOOK_PATH = os.environ['BOT_TOKEN']
+WEBHOOK_PATH = f"/{API_TOKEN}/" 
 WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
 
 # webserver settings
@@ -90,7 +90,7 @@ if __name__ == '__main__':
     if USE_WEBHOOK == '1':
         executor.start_webhook(
             dispatcher=dp,
-            webhook_path=WEBHOOK_URL,
+            webhook_path=WEBHOOK_PATH,
             on_startup=on_startup,
             on_shutdown=on_shutdown,
             skip_updates=True,
