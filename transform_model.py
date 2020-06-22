59b3f400fb54ad35db2b96bf37062f7b2dc22431
OF-Ala <66063022+OF-Ala@users.noreply.github.com>
2020-06-22 14:19:19 +0300

    Update transform_model.py

--- transform_model.py
+++ transform_model.py
@@ -238,7 +238,7 @@ class Transform_model():
     def __init__(self, path_name, state_dir):
         # все изображения будут масштабированы к размеру 1024x**** px
         # иначе расчеты идут ну очень долго
-        self.RESCALE_SIZE = 800
+        self.RESCALE_SIZE = 640
         self.save_path = path_name
         self.gen_A = Generator_net()
         self.gen_B = Generator_net()
