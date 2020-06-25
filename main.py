7637d3dcd4fc74ff655448347aa41281ae23eb8d
OF-Ala <66063022+OF-Ala@users.noreply.github.com>
2020-06-25 21:55:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -159,6 +159,7 @@ async def echo(message: types.Message):
 
 
 async def on_startup(dp):
+    await bot.delete_webhook()
     await bot.set_webhook(WEBHOOK_URL)
     # insert code here to run it after start
 
@@ -169,7 +170,7 @@ async def on_shutdown(dp):
     # insert code here to run it before shutdown
 
     # Remove webhook (not acceptable in some cases)
-    await bot.delete_webhook()
+    #await bot.delete_webhook()
 
     # Close DB connection (if used)
     await dp.storage.close()
